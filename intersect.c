5fe927bc364fea6d4d44cf5efd2d0e19f6928408
James Viner Esq <jrviner2000@gmail.com>
2023-01-20 12:59:18 -0500

    Implement comparison of words between files

--- intersect.c
+++ intersect.c
@@ -13,6 +13,7 @@ enum return_codes {
 };
 
 hash_set *load_words(FILE *fo);
+void compare_words(hash_set *set, FILE *fo, size_t file_num);
 
 int main(int argc, char *argv[])
 {
@@ -40,20 +41,32 @@ int main(int argc, char *argv[])
 	FILE *file_1 = fopen(argv[1], "r");
 	hash_set *set = load_words(file_1);
 	fclose(file_1);
+
+	int file_num = 2;
+	while(file_num < argc) {
+		FILE *fo = fopen(argv[file_num], "r");
+		compare_words(set, fo, file_num - 2);
+		fclose(fo);
+		++file_num;
+	}
 	hash_node *node = NULL;
 
 	int i = 0;
 	while(i < set->size) {
 		if (set->table[i] != NULL) {
-			printf("word[%u] == %s", i, set->table[i]->word);
-			if (set->table[i]->alt_next != NULL) {
-				node = set->table[i]->alt_next;
-				while(node != NULL) {
-					printf(", alt: %s", node->word);
-					node = node->alt_next;
+			// Safe to cast file_num as unsigned, as it will never
+			// be less than 0
+			if(set->table[i]->counter == (unsigned) file_num - 2) {
+				printf("word[%u] == %s", i, set->table[i]->word);
+				if (set->table[i]->alt_next != NULL) {
+					node = set->table[i]->alt_next;
+					while(node != NULL) {
+						printf(", alt: %s", node->word);
+						node = node->alt_next;
+					}
 				}
+				putchar('\n');
 			}
-			putchar('\n');
 		}
 		++i;
 	}
@@ -87,4 +100,26 @@ hash_set *load_words(FILE *fo)
 		free(line_buf);
 	}
 	return(set);
+}
+
+void compare_words(hash_set *set, FILE *fo, size_t file_num)
+{
+	char *line_buf = NULL;
+	size_t buf_size = 0;
+	while(getline(&line_buf, &buf_size, fo) != -1) {
+		if (line_buf[0] == '\n') {
+			continue;
+		}
+		char *current_word = strtok(line_buf, " \t\n\v\f\r");
+		if (current_word != NULL) {
+			hash_set_add_alts(set, current_word, file_num);
+		}
+		while ((current_word = strtok(NULL, " \t\n\v\f\r")) != NULL) {
+			hash_set_add_alts(set, current_word, file_num);
+		}
+	}
+	if (line_buf) {
+		free(line_buf);
+	}
+	return;
 }
